a8d2eb509643d263f06ebc5d5bf98deb91fdef6c
binghan60 <binghan60@gmail.com>
2022-08-29 20:45:59 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,46 @@
+const data = {
+  products: [
+    {
+      category: "類別",
+      name: "鮭魚",
+      slug: "salmon",
+      price: 200,
+      description: "產品描述",
+      img: "product1.jpg",
+      countInStock: 10,
+      rating: 4.5,
+    },
+    {
+      category: "類別",
+      name: "干貝",
+      slug: "salmon1",
+      price: 300,
+      description: "產品描述",
+      img: "product2.jpg",
+      countInStock: 10,
+      rating: 4.5,
+    },
+    {
+      category: "類別",
+      name: "帆立貝",
+      slug: "salmon2",
+      price: 250,
+      description: "產品描述",
+      img: "product3.jpg",
+      countInStock: 10,
+      rating: 4.5,
+    },
+    {
+      category: "類別",
+      name: "產品名稱",
+      slug: "salmon3",
+      price: 100,
+      description: "產品描述",
+      img: "product1.jpg",
+      countInStock: 10,
+      rating: 4.5,
+    },
+  ],
+};
+
+export default data;
