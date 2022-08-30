736fed4c3d947693b487852330bd059fbfab4a2e
binghan60 <binghan60@gmail.com>
2022-08-30 19:14:23 +0800

    API

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 import express from "express";
 import data from "./data.js";
+import cors from "cors";
 
 const app = express();
+
+// Top-level middlewares
+const corsOptions = {
+  // 全部允許
+  credentials: true,
+  origin: (origin, cb) => {
+    cb(null, true);
+  },
+};
+app.use(cors(corsOptions));
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
@@ -9,5 +20,5 @@ app.get("/api/products", (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`server at http://localhost:${port}`);
+  console.log(`server start at http://localhost:${port}`);
 });
