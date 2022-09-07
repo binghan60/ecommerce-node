8ab9b71bd9973e11933a7255cc92fc936263cb99
binghan60 <binghan60@gmail.com>
2022-09-07 20:29:11 +0800

    從mongo抓data

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import data from "./data.js";
 import cors from "cors";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
 
-//讀取設定檔
+//讀取連線設定檔
 dotenv.config();
 
 mongoose
@@ -27,26 +29,10 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-app.get("/api/products/slug/:slug", (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.send({ message: "找不到該產品" });
-  }
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id == req.params.id);
-  //x.id是字串只能兩個==
-  if (product) {
-    res.send(product);
-  } else {
-    res.send({ message: "找不到該產品" });
-  }
-});
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
