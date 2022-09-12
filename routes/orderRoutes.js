ba275ffe552efd0b5153ce0ed92d28715c305353
binghan60 <binghan60@gmail.com>
2022-09-12 23:36:57 +0800

    訂單付款(修改狀態)

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -38,5 +38,28 @@ orderRouter.get(
     }
   })
 );
+//更新付款狀態
+//用params ID去抓訂單 並修改其中幾項
+orderRouter.put(
+  "/:id/pay",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.email_address,
+      };
+      const updatedOrder = await order.save();
+      res.send({ message: "訂單已付款", order: updatedOrder });
+    } else {
+      res.status(404).send({ message: "找不到該訂單" });
+    }
+  })
+);
 
 export default orderRouter;
--- server.js
+++ server.js
@@ -35,6 +35,10 @@ app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use("/api/keys/paypal",(req,res)=>{
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb")
+})
+
 app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
