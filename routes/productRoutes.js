ebcdb06726e9bb1fb30d04b4fc5c832e990b750e
binghan60 <binghan60@gmail.com>
2022-09-18 23:13:05 +0800

    mongooseDB做篩選

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import expressAsyncHandler from "express-async-handler";
 import Product from "../models/productModel.js";
 
 const productRouter = express.Router();
@@ -7,6 +8,104 @@ productRouter.get("/", async (req, res) => {
   const products = await Product.find();
   res.send(products);
 });
+const PAGE_SIZE = 6;
+productRouter.get(
+  "/search",
+  expressAsyncHandler(async (req, res) => {
+    const { query } = req;
+    const pageSize = query.pageSize || PAGE_SIZE;
+    const page = query.page || 1;
+    const category = query.category || "";
+    const price = query.price || "";
+    const rating = query.rating || "";
+    const order = query.order || "";
+    //搜尋欄位的值
+    const searchQuery = query.query || "";
+
+    //有篩選條件且非all
+    const queryFilter =
+      searchQuery && searchQuery !== "all"
+        ? {
+          //regex  "i" 不區分大小寫
+            name: {
+              $regex: searchQuery,
+              $options: "i",
+            },
+          }
+        : {};
+    const categoryFilter = category && category !== "all" ? { category } : {};
+    const ratingFilter =
+      rating && rating !== "all"
+        ? {
+            rating: {
+              $gte: Number(rating),
+            },
+          }
+        : {};
+    const priceFilter =
+      price && price !== "all"
+        ? {
+            // 1-50 用減號做分隔
+            //greater than or equal 大於等於
+            //less than or equal    小於等於
+            price: {
+              $gte: Number(price.split("-")[0]),
+              $lte: Number(price.split("-")[1]),
+            },
+          }
+        : {};
+    const sortOrder =
+      order === "featured"
+        ? { featured: -1 }
+        : order === "lowest"
+        ? { price: 1 }
+        : order === "highest"
+        ? { price: -1 }
+        : order === "toprated"
+        ? { rating: -1 }
+        : order === "newest"
+        ? { createdAt: -1 }
+        : { _id: -1 };
+
+    const products = await Product.find({
+      ...queryFilter,
+      ...categoryFilter,
+      ...priceFilter,
+      ...ratingFilter,
+    })
+      .sort(sortOrder)
+      .skip(pageSize * (page - 1))
+      .limit(pageSize);
+
+    const countProducts = await Product.countDocuments({
+      ...queryFilter,
+      ...categoryFilter,
+      ...priceFilter,
+      ...ratingFilter,
+    });
+    res.send({
+      products,
+      countProducts,
+      page,
+      pages: Math.ceil(countProducts / pageSize),
+    });
+  })
+);
+
+productRouter.get(
+  "/brands",
+  expressAsyncHandler(async (req, res) => {
+    const brands = await Product.find().distinct("brand");
+    res.send(brands);
+  })
+);
+productRouter.get(
+  "/categories",
+  expressAsyncHandler(async (req, res) => {
+    const categories = await Product.find().distinct("category");
+    res.send(categories);
+  })
+);
 
 //從列表進入指定slug的產品頁
 productRouter.get("/slug/:slug", async (req, res) => {
