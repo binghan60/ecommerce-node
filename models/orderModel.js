6ff74adc455be29640bfcf5b0434aff46672b68a
binghan60 <binghan60@gmail.com>
2022-09-23 04:47:25 +0800

    修改錯字

--- models/orderModel.js
+++ models/orderModel.js
@@ -38,7 +38,7 @@ const orderSchema = new mongoose.Schema(
     isPaid: { type: Boolean, default: false },
     paidAt: { type: Date },
     isDelivered: { type: Boolean, default: false },
-    deliverdAt: { type: Date },
+    deliveredAt: { type: Date },
   },
   {
     timestamps: true,
