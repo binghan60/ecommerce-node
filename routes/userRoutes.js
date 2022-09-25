ea83819f36149eed7fa0d30b5e3d3c3f25a8cdb0
binghan60 <binghan60@gmail.com>
2022-09-26 00:39:44 +0800

    用戶編輯

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,16 +2,40 @@ import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import bcrypt from "bcryptjs";
 import User from "../models/userModel.js";
-import { generateToken, isAuth } from "../utils.js";
+import { generateToken, isAdmin, isAuth } from "../utils.js";
 
 const userRouter = express.Router();
+
+userRouter.get(
+  "/",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const users = await User.find();
+    res.send(users);
+  })
+);
+
+userRouter.get(
+  "/:id",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id); //用param的ID去資料庫找用戶
+    if (user) {
+      res.send(user); //如果有就回傳
+    } else {
+      res.status(404).send({ message: "找不到用戶" });
+    }
+  })
+);
 
 userRouter.post(
   "/signin",
   expressAsyncHandler(async (req, res) => {
     //從User找尋該筆email的帳號資料
     const user = await User.findOne({ email: req.body.email });
-    //假如有該用戶
+    //確認有該用戶
     if (user) {
       //比較加密過後的密碼是否一樣
       if (bcrypt.compareSync(req.body.password, user.password)) {
@@ -74,5 +98,59 @@ userRouter.put(
     }
   })
 );
+userRouter.put(
+  "/:id",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
 
+    if (req.user._id === user._id.toString()) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.isAdmin = req.body.isAdmin;
+      const updatedUser = await user.save();
+      console.log(updatedUser);
+      res.send({
+        message: "修改成功",
+        user: {
+          _id: updatedUser._id,
+          name: updatedUser.name,
+          email: updatedUser.email,
+          isAdmin: updatedUser.isAdmin,
+          token: generateToken(updatedUser),
+        },
+      });
+      return;
+    }
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.isAdmin = req.body.isAdmin;
+      const updatedUser = await user.save();
+      res.send({ message: "修改成功", user: updatedUser });
+    } else {
+      res.status(404).send({ message: "找不到用戶" });
+    }
+  })
+);
+
+userRouter.delete(
+  "/:id",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user.isAdmin) {
+      res.status(404).send({ message: "管理者帳號不可刪除" });
+      return;
+    }
+    if (user) {
+      await user.remove();
+      res.send({message:"刪除成功"})
+    } else {
+      res.status(404).send({message:"找不到用戶"})
+    }
+  })
+);
 export default userRouter;
